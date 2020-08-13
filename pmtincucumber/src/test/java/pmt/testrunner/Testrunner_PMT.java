4538c4f400a68c8f01b288bec7b5cc85d81d2fe9
arun30 <44123805+arun30@users.noreply.github.com>
2020-08-13 21:14:15 +0530

    Update Testrunner_PMT.java

--- pmtincucumber/src/test/java/pmt/testrunner/Testrunner_PMT.java
+++ pmtincucumber/src/test/java/pmt/testrunner/Testrunner_PMT.java
@@ -15,7 +15,7 @@ import cucumber.api.junit.Cucumber;
 			//	plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html"},
 				//plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
 		strict = false,
-				tags = {"@Buyersguide"}
+				tags = {"@Newstuck"}
 		
 		)
 
